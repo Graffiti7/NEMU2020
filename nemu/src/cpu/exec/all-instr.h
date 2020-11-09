b1969fd763f1c84272893f0046fa4f6da6a94b9d
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 15:32:59 +0000

    >  compile NEMU 3019244242 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:32:59 up 40 min,  1 user,  load average: 1.06, 1.09, 0.96 2c95d35374fd96cc280009f3bde51b669c09984b

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -2,9 +2,30 @@
 
 #include "data-mov/mov.h"
 #include "data-mov/xchg.h"
-#include "data-mov/movext.h"
+#include "data-mov/push.h"
+#include "data-mov/pop.h"
 #include "data-mov/cltd.h"
 #include "data-mov/leave.h"
+#include "data-mov/movext.h"
+#include "data-mov/movzb.h"
+#include "data-mov/movzw.h"
+#include "data-mov/movsx.h"
+
+#include "con-trans/call.h"
+#include "con-trans/je.h"
+#include "con-trans/ret.h"
+#include "con-trans/jmp.h"
+#include "con-trans/jbe.h"
+#include "con-trans/jne.h"
+#include "con-trans/jle.h"
+#include "con-trans/jg.h"
+#include "con-trans/jl.h"
+#include "con-trans/jge.h"
+#include "con-trans/ja.h"
+#include "con-trans/js.h"
+#include "con-trans/jns.h"
+#include "con-trans/jb.h"
+#include "con-trans/jae.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
@@ -13,85 +34,29 @@
 #include "arith/mul.h"
 #include "arith/idiv.h"
 #include "arith/div.h"
+#include "arith/cmp.h"
+#include "arith/add.h"
+#include "arith/adc.h"
+#include "arith/sub.h"
+#include "arith/sbb.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
 #include "logic/not.h"
-
 #include "logic/xor.h"
-
 #include "logic/sar.h"
-
 #include "logic/shl.h"
-
 #include "logic/shr.h"
-
 #include "logic/shrd.h"
+#include "logic/test.h"
+#include "logic/setne.h"
 
 #include "string/rep.h"
+#include "string/movs.h"
+#include "string/lods.h"
+#include "string/scas.h"
+#include "string/stos.h"
 
 #include "misc/misc.h"
 
 #include "special/special.h"
-
-#include"con-trans/call.h"
-
-#include"data-mov/push.h"
-
-#include"logic/test.h"
-
-#include"con-trans/je.h"
-
-#include"arith/cmp.h"
-
-#include"data-mov/pop.h"
-
-#include"con-trans/ret.h"
-
-#include"arith/add.h"
-
-#include"arith/sub.h"
-
-#include"string/lods.h"
-
-#include"string/scas.h"
-
-#include"con-trans/jne.h"
-
-#include"con-trans/jmp.h"
-
-#include"arith/sbb.h"
-
-#include"con-trans/jle.h"
-
-#include"string/movs.h"
-
-#include"string/stos.h"
-
-#include"con-trans/jbe.h"
-
-#include"con-trans/jg.h"
-
-#include"con-trans/js.h"
-
-#include"con-trans/jns.h"
-
-#include"con-trans/jge.h"
-
-#include"con-trans/jl.h"
-
-#include"arith/adc.h"
-
-#include"con-trans/jb.h"
-
-#include"con-trans/ja.h"
-
-#include"con-trans/jae.h"
-
-#include"logic/setne.h"
-
-#include"data-mov/movsx.h"
-
-#include"data-mov/movzb.h"
-
-#include"data-mov/movzw.h"
